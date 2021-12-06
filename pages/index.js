4ba3b9833e99ea4bb1b61d7219f7ef3987d91080
Justin Morrison <morrisonak@yahoo.com>
2021-12-05 18:16:36 -0600

    DB add

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import matter from 'gray-matter'
 import Head from 'next/head'
 import Post from '../components/Post'
 import { sortByDate } from '../utils'
-
+//import clientPromise from '../lib/mongodb'
 export default function Home({ posts }) {
+  console.log()
   return (
     <div>
       <Head>
@@ -20,6 +21,23 @@ export default function Home({ posts }) {
     </div>
   )
 }
+
+//export async function getServerSideProps(context) {
+ // const client = await clientPromise
+
+  // client.db() will be the default database passed in the MONGODB_URI
+  // You can change the database by calling the client.db() function and specifying a database like:
+  // const db = client.db("myDatabase");
+  // Then you can execute queries against your database like so:
+  // db.find({}) or any of the MongoDB Node Driver commands
+
+  //const isConnected = await client.isConnected()
+
+  //return {
+ //   props: { isConnected },
+  //}
+//}
+
 
 export async function getStaticProps() {
   // Get files from the posts dir
